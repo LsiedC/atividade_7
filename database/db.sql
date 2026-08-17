db965499c5f412b510c507c1038568b583012ff9
LsiedC <lucas_correa14@estudante.sesisenai.org.br>
2026-08-17 10:12:23 -0300

    up - comecei a criar o conteudo de db

---
+++ database/db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE atividade_7;
+USE atividade_7;
+
+CREATE TABLE usuario (
+    idUser INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR (87) NOT NULL,
+    e-mail VARCHAR(254) NOT NULL UNIQUE
+)
+
+CREATE TABLE pratos (
+    idPrato INT AUTO_INCREMENT PRIMARY KEY,
+    nomePrato VARCHAR (87)
+)

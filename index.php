1afad3152b9e7946db4b6845c91ef678b5e02b29
LsiedC <lucas_correa14@estudante.sesisenai.org.br>
2026-08-17 11:36:16 -0300

    up - atualizei algumas questões referentes a href das paginas, index agora é uma página de redirecionamento para menuPrincipal.php

--- index.php
+++ index.php
@@ -2,22 +2,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <script>
+        window.location.replace("public/menuPrincipal.php");
+    </script>
+    <title>Redirecionando...</title>
 </head>
 <body>
-    <main>
-        <h1>O que você deseja fazer? </h1>
-        <a href="public/cadastrarUsuario.php">Cadastrar Usuario</a>
-        <br>
-        <a href="public/cadastrarPrato.php">Cadastrar Prato</a>
-        <br>
-        <a href="public/listarPrato.php">Listar Pratos</a>
-        <br>
-        <a href="public/listarPratoUsuario.php">Listar Pratos por Usuario</a>
-        <br>
-        <a href="public/editar.php">Editar Pratos</a>
-        <br>
-        <a href="public/excluir.php">Excluir Pratos</a>
-    </main>
+    
 </body>
 </html>
---
+++ public/menuPrincipal.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <h1>O que você deseja fazer? </h1>
+        <a href="cadastrarUsuario.php">Cadastrar Usuario</a>
+        <br>
+        <a href="cadastrarPrato.php">Cadastrar Prato</a>
+        <br>
+        <a href="listarPrato.php">Listar Pratos</a>
+        <br>
+        <a href="listarPratoUsuario.php">Listar Pratos por Usuario</a>
+        <br>
+        <a href="editar.php">Editar Pratos</a>
+        <br>
+        <a href="excluir.php">Excluir Pratos</a>
+    </main>
+</body>
+</html>

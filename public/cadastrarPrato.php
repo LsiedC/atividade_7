fd2c1a6d59ed7d199ccc5727a7e04b43900191a8
LsiedC <lucas_correa14@estudante.sesisenai.org.br>
2026-08-19 11:59:01 -0300

    up - terminei a parte de prepared statement

--- public/cadastrarPrato.php
+++ public/cadastrarPrato.php
@@ -6,10 +6,12 @@ $nomePrato = $_POST["nomePrato"];
 $preco = $_POST["preco"];
 $categoria = $_POST["categoria"];
 
-$sql = "INSERT INTO pratos (nomePrato, preco, categoria) VALUES('$nomePrato', '$preco', '$categoria')";
+$sql = "INSERT INTO pratos (nomePrato, preco, categoria) VALUES('?', '?','?')";
 
-mysqli_query($conexao, $sql);
-
+$stmt = mysqli_prepare($conexao, $sql);
+mysqli_stmt_bind_param($stmt, "sss", $nomePrato, $preco, $categoria);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_close($stmt);
 }
 
 ?>
@@ -22,23 +24,30 @@ mysqli_query($conexao, $sql);
     <title>Document</title>
 </head>
 <body>
-    <main>
-        <a href="menuPrincipal.php">Voltar para tela principal</a>
-    </main>
     <form action="" method = "POST">
 
+        <label for="nomePrato">Nome:</label>
+        <br>
         <input type="text" id = "nomePrato" name= "nomePrato"> 
         <br>
+        <label for="preco">Preço:</label>
+        <br>
         <input type="text" id = "preco" name = "preco"> 
+        <br>
+        <label for="categoria">Categoria do que você quer:</label>
         <br>
         <select name="categoria" required>
             <option value="principal">Prato Principal</option>
             <option value="sobremesa">Sobremesa</option>
             <option value="bebida">Bebida</option>
         </select>
+        <br>
 
        <br> 
        <button type="submit"> Enviar </button> 
+       <br>
+       <a href="menuPrincipal.php">Voltar para tela principal</a>
+
     </form>
 </body>
 </html>

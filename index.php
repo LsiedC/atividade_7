25790bbe91c26c37de7c3db1ad3aa1ce54771134
HenriqueVenso <henrique_venso@estudante.sesisenai.org.br>
2026-08-19 09:12:05 -0300

    add - Fiz o index e o listamento dos cardápios

--- index.php
+++ index.php
@@ -1,13 +1,46 @@
+<?php
+
+include "infra/conexao.php";
+$pratos = mysqli_query($conexao, "SELECT * FROM pratos");
+
+?>
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script>
-        window.location.replace("public/menuPrincipal.php");
-    </script>
-    <title>Redirecionando...</title>
+    <title> Menu Principal</title>
 </head>
 <body>
-    
+
+    <h2> Cardápio do Dia</h2>
+
+<table>
+
+<tr>
+
+    <th> ID </th>
+    <th> Prato </th>
+    <th> Preço </th>
+    <th> Categoria </th>
+
+</tr>
+<?php while($prato = mysqli_fetch_assoc($pratos)) { ?>
+<tr>
+
+    <td> <?php echo $prato["idPrato"] ?></td>
+    <td> <?php echo $prato["nomePrato"] ?></td>
+    <td> <?php echo $prato["preco"] ?></td>
+    <td> <?php echo $prato["categoria"] ?></td>
+
+</tr>
+
+<?php } ?>
+
+</table>
+
+
+
 </body>
 </html>
